7b0e6931356614a80578776707c10bf5014c9e37
Michael Pirlis <120146118+the0megastar@users.noreply.github.com>
2025-06-19 13:16:43 -0400

    Updating Directory Structure

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const header = document.querySelector('.main-header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 10) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
